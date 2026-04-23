65361ab90531590feeadaf45a3a7401c91435561
zhangsgithub04 <ssen_zhang@hotmail.com>
2026-04-23 12:41:26 -0400

    Update test_president.py

--- test_president.py
+++ test_president.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 from unittest.mock import patch, MagicMock
-from main import app  # your FastAPI file
+from myserver import app  # your FastAPI file
 
 client = TestClient(app)
 
